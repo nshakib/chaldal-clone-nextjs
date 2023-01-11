48643b31de601cfc0fc202a249da07cd3537c98f
nshakib <nazmus.shakib.cse@gmail.com>
2023-01-12 03:32:10 +0600

    extra file deleted

--- src/button-group.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const button-group = () => {
-  return (
-    <div>button-group</div>
-  )
-}
-
-export default button-group
--- src/seo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const seo = () => {
-  return (
-    <div>seo</div>
-  )
-}
-
-export default seo
