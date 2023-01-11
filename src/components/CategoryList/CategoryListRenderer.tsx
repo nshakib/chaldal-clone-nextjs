24ec1b76e3aeaaaf634d1387934531685b60f1a0
nshakib <nazmus.shakib.cse@gmail.com>
2023-01-12 03:22:13 +0600

    bug fixed

--- src/components/CategoryList/CategoryListRenderer.tsx
+++ src/components/CategoryList/CategoryListRenderer.tsx
@@ -3,19 +3,20 @@ import { SkeletonListContext } from '../../context'
 import SkeletonList from '../SkeletonList/SkeletonList'
 
 type CategoryListRendererProps = {
-    categories: ProductCategory[];
-    breakpoints: Breakpoints;
+    // categories: ProductCategory[];
+    // breakpoints: Breakpoints;
   }
 
-const CategoryListRenderer = ({categories,breakpoints}: CategoryListRendererProps) => {
+const CategoryListRenderer = () => {
   return (
-    <SkeletonListContext.Provider value={breakpoints}>
-      {categories.length === 0 ? (
-        <SkeletonList itemCount={3} />
-      ) : (
-        <CategoryList categories={categories} />
-      )}
-    </SkeletonListContext.Provider>
+    // <SkeletonListContext.Provider value={breakpoints}>
+    //   {/* {categories.length === 0 ? (
+    //     <SkeletonList itemCount={3} />
+    //   ) : (
+    //     <CategoryList categories={categories} />
+    //   )} */}
+    // </SkeletonListContext.Provider>
+    <></>
   )
 }
 
